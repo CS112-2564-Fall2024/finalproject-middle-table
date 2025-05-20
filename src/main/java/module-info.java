e1b4312645aec2a06c6224de50b654b3327c5b0b
KlowasMunik <Munikgklowas@gmail.com>
2025-05-20 13:31:10 -0700

    whole project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,17 @@
 module edu.miracosta.cs112.finalproject.finalproject {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.desktop;
+    requires javafx.media;
 
     opens edu.miracosta.cs112.finalproject.finalproject to javafx.fxml;
     exports edu.miracosta.cs112.finalproject.finalproject;
+    exports edu.miracosta.cs112.finalproject.finalproject.Items;
+    opens edu.miracosta.cs112.finalproject.finalproject.Items to javafx.fxml;
+    exports edu.miracosta.cs112.finalproject.finalproject.Entities;
+    opens edu.miracosta.cs112.finalproject.finalproject.Entities to javafx.fxml;
+    exports edu.miracosta.cs112.finalproject.finalproject.lib;
+    opens edu.miracosta.cs112.finalproject.finalproject.lib to javafx.fxml;
+    exports edu.miracosta.cs112.finalproject.finalproject.controllers;
+    opens edu.miracosta.cs112.finalproject.finalproject.controllers to javafx.fxml;
 }
